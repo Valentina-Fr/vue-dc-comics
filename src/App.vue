40b27cb487a6e3794717b3940f935de006239fb9
Valentina-Fr <v_frasca@virgilio.it>
2021-09-06 17:18:18 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from '@/components/Header.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+img {
+  max-width: 100%;
+}
+
+a {
+  text-decoration: none;
+  padding: 0 5px;
+  color: darkgray;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <div class="col-4">
+            <img src="@/assets/img/dc-logo.png" alt="logo" id="logo">
+        </div>
+        <div class="col-8">
+            <ul>
+                <li v-for="(link, index) in links" :key="index"><a :href="link.url">{{link.text}}</a></li>
+            </ul>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+            links: [
+  {
+    text: 'Characters',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Comics',
+    url: '#',
+    current: true,
+  },
+  {
+    text: 'Movies',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'TV',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Games',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Collectibles',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Videos',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Fans',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'News',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Shop',
+    url: '#',
+    current: false,
+  },
+]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .row {
+        display: flex;
+        align-items: center;
+    }
+
+    .col-8 {
+        width: calc(100% / 12 * 8);
+    }
+
+    .col-4{
+        width: calc(100% / 12 * 4);
+    }
+
+    #logo {
+        padding: 10px 0;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        text-transform: uppercase;
+    }
+    
+</style>
